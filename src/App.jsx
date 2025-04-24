5b9c5428d839aa261cbe9aab86336fe044b242c6
GenerationJava <generation.chilec11@gmail.com>
2025-04-24 15:47:41 -0400

    Modo oscuro con useContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import './App.css'
 import Nosotros from './components/Nosotros/Nosotros'
 import Navbar from './components/Navbar/Navbar'
+import Footer from './components/Footer/Footer'
+import { ThemeProvider } from './context/themeContext'
 
 function App() {
 
 
   return (
     <>
-     <Navbar/>
-      <Nosotros/>
+    <ThemeProvider>
+        <div className="App">
+          <Navbar />
+            <div className='nosotros-container'>
+              <Nosotros/>
+            </div>
+          {/* Otros componentes */}
+          <Footer />
+        </div>
+      </ThemeProvider>
     </>
   )
 }
